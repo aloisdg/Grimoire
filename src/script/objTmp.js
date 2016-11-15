219145c7300654b879a8459de7332577dec78e0a
aloisdg <aloisdegouvello@live.fr>
2016-11-15 22:42:01 +0100

    Clean scripts

--- src/script/objTmp.js
+++ src/script/objTmp.js
@@ -1,50 +1,6 @@
-function chunk(chunkSize, array) {
-    return array.reduce(function(previous, current) {
-        let chunk;
-        if (previous.length === 0 ||
-                previous[previous.length - 1].length === chunkSize) {
-            chunk = [];   // 1
-            previous.push(chunk);   // 2
-        }
-        else
-            chunk = previous[previous.length -1];   // 3
-        chunk.push(current);   // 4
-        return previous;   // 5
-    }, []);   // 6
-}
-
-// 
-// Segoe MDL2 Assests
-// var time = {
-//     color: '#265E47',
-//     title: 'Arrêt du temps',
-//     school: 'Transmutation',
-//     schoolFull: 'Transmutation',
-//     effect: 'Seul le PJ peut agir pendant 1d4+1 rounds.',
-//     image: {
-//         backgroundImage: "url('time.jpg')"
-//     },
-//     background: {
-//         backgroundImage: "url('Transmutation.svg')"
-//     }
-// };
-// var fireball = {
-//     color: '#265E47',
-//     title: 'Boule de feu',
-//     school: 'Evocation',
-//     schoolFull: 'Évocation (feu)',
-//     effect: "1d6 points de dégâts de feu/niveau, 6 m de rayon.",
-//     image: {
-//         backgroundImage: "url('fireball.jpg')"
-//     },
-//     background: {
-//         backgroundImage: "url('Evocation.svg')"
-//     }
-// };
-
 // http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Mains%20br%c3%bblantes.ashx
 // http://www.pathfinder-fr.org/Wiki/Pathfinder-RPG.Voile%20du%20paradis.ashx
-var door = {
+const door = {
     color: '#265E47',
     title: 'Porte dimensionnelle',
     school: 'Invocation',
@@ -92,45 +48,11 @@ Vue.component('book', {
     }
 });
 
-let spells = chunk(9, Array.from({length: 20}, () => door));
+const spells = chunk(9, repeat(door));
 
 new Vue({
     el: '.book',
     data: {
         co: spells
     }
-    //   el: '#app',
-    //   data: door,
-    //     components: {
-    //     // <my-component> will only be available in parent's template
-    //     'my-component': Child
-    //   }
 });
-
-
-// function loadJSON(callback) {
-//     var xobj = new XMLHttpRequest();
-//     xobj.overrideMimeType("application/json");
-//     xobj.open('GET', 'script/spells.json', true); // Replace 'my_data' with the path to your file
-//     xobj.onreadystatechange = function () {
-//         if (xobj.readyState == 4 && xobj.status == "200") {
-//             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-//             callback(xobj.responseText);
-//         }
-//     };
-//     xobj.send(null);
-// }
-
-// function init() {
-//     loadJSON(function (response) {
-//         // Parse JSON string into object
-//         var spells = JSON.parse(response);
-
-//         // for (var i = 0; i < spells.Spells.length; i++) {
-//         //     console.log(spells.Spells[i].Name + " (" + spells.Spells[i].School+ ")");
-//         // }
-
-//     });
-// }
-
-// init();
---
+++ src/script/utility.js
@@ -0,0 +1,18 @@
+function chunk(chunkSize, array) {
+    return array.reduce((previous, current) => {
+        let chunk;
+        if (previous.length === 0
+            || previous[previous.length - 1].length === chunkSize) {
+            chunk = [];
+            previous.push(chunk);
+        }
+        else
+            chunk = previous[previous.length - 1];
+        chunk.push(current);
+        return previous;
+    }, []);
+}
+
+function repeat(item) {
+    return Array.from({ length: 20 }, () => item)
+}
