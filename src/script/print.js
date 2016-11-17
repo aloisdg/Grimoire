5266a9e8b5489ec949c51ffc2facae7d614336c1
aloisdg <aloisdegouvello@live.fr>
2016-11-17 13:36:38 +0100

    Remove zone and effet. Improve space.

--- src/script/print.js
+++ src/script/print.js
@@ -36,10 +36,10 @@ for (var i = 0; i < book.length; i++) {
         schoolFull: book[i].school,
         summary: book[i].summary,
         castingTime: book[i].castingTime,
-        components: JSON.parse(book[i].components).Kinds.split(', ').map(x => x[0]).join(', '), // why components is a string instead of {}?
+        components: book[i].components ? JSON.parse(book[i].components).Kinds.split(', ').map(x => x[0]).join(', ') : book[i].components, // why components is a string instead of {}?
         range: book[i].range ? JSON.parse(book[i].range).Unit : book[i].range, // wtf not a json?
-        effect: 'vague de 3 m (2 c) de profondeur',
-        zone: 'rayonnement en forme de cône',
+        // effect: 'vague de 3 m (2 c) de profondeur',
+        // zone: 'rayonnement en forme de cône',
         target: book[i].target ? JSON.parse(book[i].target).Value : book[i].target,
         duration: 'instantanée',
         savingThrow: book[i].savingThrow,
